cb3908861b1d78809fae566615e82dd69ebc139e
Tobias Furtado da Silva <tobias_fs_@hotmail.com>
2017-08-29 19:25:21 -0300

    Add files via upload

---
+++ LIST 5/ex2.c
@@ -0,0 +1,29 @@
+/*
+    TOBIAS FURTADO 170141X - 29.08
+
+    EXERCICIO 2.
+        Escreva um programa que imprima um vetor
+        de inteiros na ordem inversa, acessando os
+        elementos por meio de um ponteiro.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int main(void){
+    int numeros[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, contadorA;
+    int *pInteiro = &numeros[MAX-1];
+
+    printf("\n\n\tOrdem inversa: {");
+    for (contadorA = 0; contadorA < MAX; contadorA++){
+        printf("%d", *(pInteiro - contadorA));
+
+        if (contadorA != (MAX - 1)) printf(", "); else printf("}");
+    }
+
+    printf("\n\n");
+
+    return 0;
+}
